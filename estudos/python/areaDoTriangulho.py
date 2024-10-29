dcdf79b46f5a4e36d3a8e77aaa68f01cbcef61d0
Joao Nacur <luisoujpof@gmail.com>
2024-10-29 08:33:05 -0300

    Add files via upload

---
+++ estudos/python/areaDoTriangulho.py
@@ -0,0 +1,10 @@
+# base
+base = int(input("qual é o valor da base? "))
+
+altura = int(input("Qual é o valor da altura? "))
+
+multi = base * altura
+
+resultado = multi / 2
+
+print("area do triangulho ", resultado)
---
+++ estudos/python/main.py
@@ -0,0 +1,11 @@
+valor1 = int(input("Coloque um valor: "))
+
+valor2 = int(input("Segundo valor: "))
+
+soma = valor1 + valor2
+
+print ("Soma dos dois valores", soma)
+
+#str: é para armazenar palavras
+#int: para armazenar números
+#float: amrmazena números quebrados
